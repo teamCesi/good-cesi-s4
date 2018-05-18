231d5bdd04bb93bed26f0805de36e47b8601f393
Jenny13800 <skullinfire@aol.com>
2018-05-18 11:54:31 +0200

    Modification User Mise a jour du Controller / views / creation form

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,30 +2,103 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use App\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class UtilisateurController extends Controller
 {
     /**
-     * @Route("/utilisateur", name="utilisateur")
+     * @Route("/utilisateur", name="utilisateur_list")
      */
-    public function index()
+    public function indexAction(UtilisateurRepository $repo)
     {
+
+        $utilisateurs = $repo->findAll();
+
+        dump($utilisateurs);
+
         return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
+            'controller_name' => 'UtilisateurController', 
+            'utilisateurs' => $utilisateurs
         ]);
     }
-    
+
     /**
-     * @Route("/utilisateurs/{id}", name="utilisateur_show")
+     * @Route("/utilisateur/new", name="utilisateur_new")
      */
-    public function showAction(Utilisateur $user) {
+    public function createAction(Request $request, ObjectManager $manager) {
         
-        return $this->render('utilisateur/show.html.twig', [
-            'utilisateur' => $user
+        // créer un utilisateur
+        $utilisateur = new utilisateur();
+
+        // besoin du formulaire UtilisateurType
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+
+        // On doit analyser la requête HTTP
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // itégrer utilisateur à la bdd avec manager
+            $utilisateur->setIsAdmin(0);
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            return $this->redirectToRoute('utilisateur_list');
+        }
+
+        $formView = $form->createView();
+        // Aficher la vue
+        return $this->render('utilisateur/create.html.twig', [
+            'form' => $formView
         ]);
     }
-    
+
+    /**
+     * @Route("/utilisateur/{id}/edit", name="utilisateur_edit")
+     */
+    public function editAction(Utilisateur $utilisateur, ObjectManager $manager, Request $request){
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            return $this->redirectToRoute('utilisateur_show', [
+                'id' => $utilisateur->getId()
+            ]);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render("utilisateur/edit.html.twig", [
+            'form' => $formView
+        ]);
+    }
+
+    /**
+     * @Route("/utilisateur/{id}", name="utilisateur_show")
+     */
+    public function showAction(Utilisateur $utilisateur){
+        return $this->render('utilisateur/show.html.twig', [
+            'utilisateur' => $utilisateur
+        ]);
+    }
+
+    /**
+     * @Route("utilisateur/{id}/delete", name="utilisateur_delete")
+     */
+    public function deleteAction(Utilisateur $utilisateur, ObjectManager $manager) {
+        
+        $manager->remove($utilisateur);
+        $manager->flush();
+
+        return $this->redirectToRoute('utilisateur_list');
+    }
 }
---
+++ src/Form/UtilisateurType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('nom')
+        ->add('prenom')
+        ->add('username')
+        ->add('password')
+        ->add('email')
+        ->add('adresse')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
